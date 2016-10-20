04e2699d606f556f0fa71eb246a8f9c7e48d0438
asmsuechan <suenagaryoutaabc@gmail.com>
2016-10-20 21:58:03 +0900

    changeイベントで消す要素を修正

--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
 var pdf = require('./pdf')
 var pdfBtn = document.getElementById("selfile")
 var inputTime = document.getElementById("pagingTime")
+var inputDiv = document.getElementById("inputs")
 var pagingTime = 5000
 var file
 
 pdfBtn.addEventListener("change", function(evt){
   pagingTime = document.getElementById("pagingTime").value * 1000
 
-  pdfBtn.style.display = 'none'
-  inputTime.style.display = 'none'
+  inputDiv.style.display = 'none'
 
   file = evt.target.files
   localPdf = pdf.getPdf(file[0].path)
